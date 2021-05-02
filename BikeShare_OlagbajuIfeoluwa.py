0f9fadcf19d4a974d88ce29d6b137c363f9a9b2a
Olagbaju Ifeoluwa <topzy12@yahoo.com>
2021-05-02 13:31:42 +0100

    Add comment for city filter method

--- BikeShare_OlagbajuIfeoluwa.py
+++ BikeShare_OlagbajuIfeoluwa.py
@@ -5,7 +5,8 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+              
+#this method is used for getting a valid city name from the user
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
