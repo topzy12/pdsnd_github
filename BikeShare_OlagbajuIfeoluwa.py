12fa6b7ad82b7cba9ba85f9e438c1c59ffb8cc93
Olagbaju Ifeoluwa <topzy12@yahoo.com>
2021-05-02 13:55:02 +0100

    Add comment for trip_duration_stats method

--- BikeShare_OlagbajuIfeoluwa.py
+++ BikeShare_OlagbajuIfeoluwa.py
@@ -124,7 +124,7 @@ def station_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+    # this method displays the statistics on the total and average trip
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
